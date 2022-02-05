aa3c2b75c579d0708ff55eae9c3d2847b290c183
Siddarth Karki <karkisiddarth@gmail.com>
2022-02-05 22:24:04 +0530

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -1,8 +1,8 @@
-//done
 #include "ClusterCreate.h"
 #include <unistd.h>
 
 int main(){
+    // this sleep is not necessarily required, but helps in debugging on the server side
     sleep(2);
     char addr[25] = "127.0.0.1";
     runclient(addr ,8080);
